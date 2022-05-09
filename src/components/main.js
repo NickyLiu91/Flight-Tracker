f85e147c66b52a49bf99145a7a7478523023393f
Nicky <nickyliu91@gmail.com>
2022-05-09 02:21:45 -0400

    changed fetch to use env variable

--- src/components/main.js
+++ src/components/main.js
@@ -3,22 +3,27 @@ import Ticket from './ticket'
 
 class Main extends React.Component {
 
+  // this.state = {
+  //   flights: []
+  // }
+
   componentDidMount = () => {
-    // console.log(process.env.REACT_APP_KEY)
-    // console.log(process.env.REACT_APP_SECRET)
     this.fetchApiOne()
-    // this.getToken()
   }
 
   fetchApiOne = () => {
     fetch('https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=JFK&destinationLocationCode=HND&departureDate=2022-11-05&adults=1&nonStop=false&max=50', {
       method: 'GET',
       headers: {
-        Authorization: `Bearer wyfEZvHYJuWSemS15T9qM41FSkJf`
+        Authorization: `Bearer ${process.env.REACT_APP_ACCESS}`
       }
     })
     .then(res => res.json())
-    .then(json => console.log(json))
+    // .then(json => {
+    //   this.setState({
+    //     flights: json.data
+    //   })
+    // })
   }
 
   render () {
