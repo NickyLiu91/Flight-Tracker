ceafc92844bcb745b1ab46f660215d4fb8e69fdc
Nicky <nickyliu91@gmail.com>
2022-05-09 03:07:46 -0400

    fixed tickets reading variables

--- src/components/ticket.js
+++ src/components/ticket.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 const Ticket = (props) => {
+  console.log(props.ticket)
   return (
     <div className="ticket">
       <p>Final Ticket Date: {props.ticket.lastTicketingDate}</p>
       <p>Price: {props.ticket.price.total} {props.ticket.price.currency}</p>
       <p>Number of seats: {props.ticket.numberOfBookableSeats}</p>
-      <p>Oneway: {props.ticket.oneWay}</p>
-      <p>Type: {props.ticket.flightOffer}</p>
-      <p>Source: {props.ticket.GDS}</p>
+      <p>Oneway: {`${props.ticket.oneWay}`}</p>
+      <p>Type: {props.ticket.type}</p>
+      <p>Source: {props.ticket.source}</p>
     </div>
   )
 }
